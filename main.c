d6193bf1bef0ca987495edcbd239ee2d3df4bed7
mesfik <fikreamesfin@gmail.com>
2022-11-19 12:14:05 +0000

    main

--- main.c
+++ main.c
@@ -117,15 +117,6 @@ int main(int argc, char *argv[])
 	}
 	free_env();
 	free_alias_list(aliases);
-	return (*exe);
-}
-/**
- * main - main entry function
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("Hello World\n");
+	printf("%d\n", *exe);
 	return (0);
 }
